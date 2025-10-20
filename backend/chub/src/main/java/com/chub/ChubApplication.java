10fe2527419e902215c6e4439916ad5aa59b9b21
SwnBae <tngksdk12@naver.com>
2025-10-20 14:41:12 +0900

    [BE]chore: Spring Boot 프로젝트 초기 설정 및 의존성 구성

---
+++ backend/chub/src/main/java/com/chub/ChubApplication.java
@@ -0,0 +1,13 @@
+package com.chub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChubApplication.class, args);
+	}
+
+}
---
+++ backend/chub/src/test/java/com/chub/ChubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
