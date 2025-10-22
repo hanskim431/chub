474d3bb96e0a5dc188d701f0e36c4c139c630ded
hjkim <kims0887@naver.com>
2025-10-22 15:32:22 +0900

    feat: front end 초기 설정 추가

--- frontend/chub/src/main.tsx
+++ frontend/chub/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./app/App.tsx";
+import "@/index.css";
+import App from "@app/App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+async function enableMocking() {
+  if (import.meta.env.VITE_API_MOCK !== "true") {
+    return;
+  }
+  const { worker } = await import("@mocks/browser.ts");
+  await worker.start();
+}
+
+enableMocking().then(() => {
+  console.log("App 렌더링 시작!");
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+  console.log("App 렌더링 완료!");
+});
