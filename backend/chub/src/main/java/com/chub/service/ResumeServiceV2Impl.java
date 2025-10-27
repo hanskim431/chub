8093035bc516425f5811bb9082ad824c73834943
llcodingll <ll.o.llwhkr@gmail.com>
2025-10-27 15:29:09 +0900

    refactor: 불필요한 log 삭제

--- backend/chub/src/main/java/com/chub/service/ResumeServiceV2Impl.java
+++ backend/chub/src/main/java/com/chub/service/ResumeServiceV2Impl.java
@@ -51,24 +51,20 @@ public class ResumeServiceV2Impl implements ResumeServiceV2 {
         if (existingResume.isPresent()) {
             String oldFileUrl = existingResume.get().getPdfUrl();
             fileStorageService.deleteFile(oldFileUrl);
-            log.info("Deleted old resume file for user {}: {}", userId, oldFileUrl);
         }
 
         // 새 파일 저장
         String newFileUrl = fileStorageService.storeFile(file, userId);
-        log.info("Stored new resume file for user {}: {}", userId, newFileUrl);
 
         // Resume 엔티티 업데이트 또는 생성
         if (existingResume.isPresent()) {
             // 기존 이력서 업데이트 (덮어쓰기)
             Resume resume = existingResume.get();
             resume.updatePdfUrl(newFileUrl);
-            log.info("Updated existing resume for user {}", userId);
         } else {
             // 새 이력서 생성
             Resume newResume = Resume.of(user, newFileUrl);
             resumeRepository.save(newResume);
-            log.info("Created new resume for user {}", userId);
         }
     }
 
@@ -80,10 +76,8 @@ public class ResumeServiceV2Impl implements ResumeServiceV2 {
 
         // 물리적 파일 삭제
         fileStorageService.deleteFile(resume.getPdfUrl());
-        log.info("Deleted resume file for user {}: {}", userId, resume.getPdfUrl());
 
         // DB에서 삭제
         resumeRepository.delete(resume);
-        log.info("Deleted resume record for user {}", userId);
     }
 }
