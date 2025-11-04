c2164f3aa1e686b3b94db1ebb2f6fdae769dd6f2
김현수 <fogy431@gmail.com>
2025-11-04 15:56:43 +0900

    Update file ChatRoomRepository.java

--- backend/chub/src/main/java/com/chub/repository/mongo/ChatRoomRepository.java
+++ backend/chub/src/main/java/com/chub/repository/mongo/ChatRoomRepository.java
@@ -2,7 +2,6 @@ package com.chub.repository.mongo;
 
 import com.chub.entity.ChatRoom;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 import java.util.Optional;
 
 public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
