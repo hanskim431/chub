189429375a46bc0283795f5d9c7a0e3e00f52a55
김현수 <fogy431@gmail.com>
2025-11-05 17:47:58 +0900

    feat(repository): 채팅 목록 조회 DAO 구현

--- backend/chub/src/main/java/com/chub/repository/mongo/ChatRoomRepository.java
+++ backend/chub/src/main/java/com/chub/repository/mongo/ChatRoomRepository.java
@@ -2,10 +2,15 @@ package com.chub.repository.mongo;
 
 import com.chub.entity.ChatRoom;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
 
     Optional<ChatRoom> findByRoomIdContainingOrderByUpdatedAtDesc(String chatRoomId);
+
+    Optional<List<ChatRoom>> findByParticipantIdsContainingOrderByUpdatedAtDesc(Long userId);
+
 
 }
